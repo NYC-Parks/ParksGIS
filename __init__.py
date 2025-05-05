6d7573ba30ef2781eb5f58aa6c8bc9922b9f2efe
Dennis Zhang <dennis.zhang@parks.nyc.gov>
2025-05-05 11:26:52 -0400

    add __init__

---
+++ __init__.py
@@ -0,0 +1,21 @@
+from .ParksGIS import (
+    GISFactory,
+    Server,
+    LayerTable,
+    LayerAppend,
+    LayerEdits,
+    LayerQuery,
+    LayerDomainNames,
+    LayerServerGen,
+)
+
+__all__ = [
+    'GISFactory',
+    'Server',
+    'LayerTable',
+    'LayerAppend',
+    'LayerEdits', 
+    'LayerQuery',
+    'LayerDomainNames',
+    'LayerServerGen'
+]
